373f8029f1222752969b723f03456b59edfb9565
Maksim ilyukovich <m.ilyukovich@tinkoff.ru>
2022-07-12 02:08:54 +0300

    step 2 : i18n

--- Makefile
+++ Makefile
@@ -5,3 +5,8 @@ run:
 	export DJANGO_SETTINGS_MODULE=task_manager.settings
 	gunicorn task_manager.wsgi
 
+translate-make:
+	django-admin makemessages -l ru
+
+translate-compile:
+	django-admin compilemessages
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -111,7 +111,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
+
+LOCALE_PATHS = (
+    './locale/',
+)
 
 TIME_ZONE = 'UTC'
 
