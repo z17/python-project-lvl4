139b83961909f20e783e0cc14e99afd71044382a
Maksim ilyukovich <m.ilyukovich@tinkoff.ru>
2022-08-16 01:52:45 +0300

    fix unused import

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.conf import settings
-from django.urls import reverse
-
 from task_manager.statuses.models import Status
 from task_manager.labels.models import Label
 from django.utils.translation import gettext_lazy as _
